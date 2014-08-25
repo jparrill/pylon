55ec4717f9337c0d711d69f4f5fa18d5afd0a104
Juan Manuel Parrilla <padajuan@gmail.com>
2014-08-25 17:15:00 +0200

    Stup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pylon",
+    version = "0.0.1",
+    author = "Juan Manuel Parrilla Madrid",
+    author_email = "padajuan@gmail.com",
+    description = ("Use psutils to send data to Graphite Backend"),
+    license = "GPLv2",
+    keywords = "psutil pylon monitoring graphite",
+    url = "http://packages.python.org/pylon",
+    packages=['pylon'],
+    long_description=read('README'),
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: DevOps',
+        'License :: OSI Approved :: GPLv2 License',
+        'Operating System :: Redhat/CentOS/Fedora',
+        'Programming Language :: Python',
+        "Topic :: Monitoring",
+    ],
+)
